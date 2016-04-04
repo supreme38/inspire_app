af15e690067e636a7ee55607db715447fd5f0e49
supreme38 <vince.liu38@gmail.com>
2016-04-04 02:36:24 -0400

    adding paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :image)
   end
 
 end
