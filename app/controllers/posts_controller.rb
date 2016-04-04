4facabd6e0b9cbbbc0518423e78b9b2c909d81bc
supreme38 <vince.liu38@gmail.com>
2016-04-04 01:37:21 -0400

    adding edit html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.update(post_parms)
+    if @post.update(post_params)
       redirect_to @post
     else
       render 'edit'
