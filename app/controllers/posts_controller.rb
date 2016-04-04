b974cdbf1782caa0a40a22943c6b972aaafef5fa
supreme38 <vince.liu38@gmail.com>
2016-04-04 01:28:58 -0400

    adding post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   def show
@@ -11,11 +14,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
     if @post.save
       redirect_to @post
     else
       render 'new'
+    end
   end
 
   def update
@@ -27,6 +30,7 @@ class PostsController < ApplicationController
   private
 
   def find_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
