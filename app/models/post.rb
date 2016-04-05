9c1881374b7750b1e26e360daeb4ffad36a12669
supreme38 <vince.liu38@gmail.com>
2016-04-05 00:29:13 -0400

    finishing css

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   # paperclip gem requirement to allow attachment/validation of files
-  has_attached_file :image, styles: { medium: "800x600>", small: "500x500>" }
+  has_attached_file :image, styles: { medium: "600x600>", small: "400x400>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
